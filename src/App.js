11d6cfa36b8aa32572955f99306adc28f44a2b5c
businessryuya <businessryuya@gmail.com>
2021-06-11 10:09:15 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const App = () => <Counter></Counter>;
+import { increment, decrement } from "./actions";
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.state);
-    this.state = { count: 0 };
-  }
-
-  handlePlusButton = () => {
-   const currentCount = this.state.count
-    this.setState( { count: currentCount + 1 })
-  }
-
-  handleButton = () => {
-   const downCount = this.state.count
-    this.setState( { count: downCount - 1 })
-  }
-
+class App extends Component {
   render() {
+    const props = this.props
+
     return (
       <>
-        <div>count: {this.state.count} </div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleButton}>-1</button>
+        <div>value: {props.value} </div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({value: state.count.value})
+// const mapDispatchToProps = dispatch => ({
+//   increment: ()=> dispatch(increment()),
+//   decrement: ()=> dispatch(decrement())
+// })
+
+const mapDispatchToProps =({increment,decrement})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "reducer";
+import { combineReducers } from "redux";
 import count from "./count";
 
 export default combineReducers({ count });
