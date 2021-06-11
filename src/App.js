d55bb7570418d16e5a020a061f2b2894309cad42
businessryuya <businessryuya@gmail.com>
2021-06-11 09:18:08 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+const App = () => <Counter></Counter>;
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+   const currentCount = this.state.count
+    this.setState( { count: currentCount + 1 })
+  }
+
+  handleButton = () => {
+   const downCount = this.state.count
+    this.setState( { count: downCount - 1 })
+  }
+
+  render() {
+    return (
+      <>
+        <div>count: {this.state.count} </div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleButton}>-1</button>
+      </>
+    );
+  }
 }
 
 export default App;
